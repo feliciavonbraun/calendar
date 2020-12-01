5b91ad4a73be60b48b39d15ea4a01c57083401dd
Jennifer Tendell <jennifer.tendell@gmail.com>
2020-12-01 19:30:14 +0100

    Final changes!

--- JavaScript/calendar.js
+++ JavaScript/calendar.js
@@ -1,46 +1,49 @@
 
+// Global scope
 let date = new Date();
 
 
-/** Start of calendar */
+/* Start of calendar */
 function calendar() {
-    renderCalendar();
+    getDecemberMonth();
     showCurrentMonth();
-    showYear();
+    showCurrentYear();
 }
 
-/* Hämta månad */
-function renderCalendar() {
+/* Get days of December */
+function getDecemberMonth() {
     
-    let fakeMonth = ['', '', '', '', '', '', '01', '02', '03', '04', '05', '06', '07', '08', '09', 
+    const december = ['', '01', '02', '03', '04', '05', '06', '07', '08', '09', 
         10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 
-        27, 28, 29, 30];
+        27, 28, 29, 30, 31];
 
-    createElementForEachDay(fakeMonth);    
+        renderCalendar(december);    
 }
 
-
-/* Skapa element för alla dagar i månad */
-function createElementForEachDay(fakeMonth) {
+/**
+ * Render calendar to update count.  
+ * @param {(string | number)[]} december 
+ */
+    function renderCalendar(december) {
     const daysContainer = document.getElementById("day-container");
     daysContainer.innerHTML = "";
 
     
-    for (let i = 0; i < fakeMonth.length; i++) {
-        const dates = fakeMonth[i];
-        let id = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + fakeMonth[i];
+    for (let i = 0; i < december.length; i++) {
+        const dates = december[i];
+        let id = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + december[i];
         const day = createDay(dates, id);
         daysContainer.appendChild(day);
     
-
-        let count = 0; /* Todos / dag */
+        // Todos / day
+        let count = 0; 
         for(const todo of todos) {
             if(todo.date == id) {
                 count++
             }
         }
 
-        /* Om det finns minst en todo, visa den/dom */
+        // Display amount of todos, if there is any
         if (count > 0){  
             const countText = document.createElement('p')
             countText.classList.add('count-of-todo-text')
@@ -50,7 +53,11 @@ function createElementForEachDay(fakeMonth) {
     }
 }
 
-/* Skapa dag div */
+/**
+ * Design and add content for each day
+ * @param {string | number} dates 
+ * @param {string} id 
+ */
 function createDay(dates, id) {
     day = document.createElement('div');
     day.classList.add('date', 'grid', 'center', 'date:hover');
@@ -65,13 +72,15 @@ function createDay(dates, id) {
 }
 
 
-/* HEADER */
-function showYear() {
+//HEADER
+
+/* Show current year in header */
+function showCurrentYear() {
     let year = date.getFullYear();
     document.getElementById('current-year').innerHTML = year;
 }
 
-
+/* Show current month in header */
 function showCurrentMonth() {
     let months = ['Januari', 'Februari', 'Mars', 'April', 'Maj', 'Juni',
         'Juli', 'Augusti', 'September', 'Oktober', 'November', 'December']
